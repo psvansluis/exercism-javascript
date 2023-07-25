6e978345bd84ab5498f1fcf9d8c0b6821e52abe8
psvansluis <psvansluis@users.noreply.github.com>
2023-07-25 23:06:49 +0200

    yacht (except four of a kind)

---
+++ yacht/yacht.js
@@ -0,0 +1,91 @@
+// @ts-check
+
+export const score = (
+  /** @type {number[]}*/ dice,
+  /** @type {string}  */ category
+) => {
+  const yacht = new Yacht(dice);
+  const snake_case = category.replace(/\s/g, "_");
+  return yacht[snake_case];
+};
+
+class Yacht {
+  #dice;
+
+  constructor(/** @type {number[]}*/ dice) {
+    this.#dice = dice;
+  }
+
+  #countValue(value) {
+    return this.#dice.filter((die) => die === value).length * value;
+  }
+
+  get ones() {
+    return this.#countValue(1);
+  }
+
+  get twos() {
+    return this.#countValue(2);
+  }
+
+  get threes() {
+    return this.#countValue(3);
+  }
+
+  get fours() {
+    return this.#countValue(4);
+  }
+
+  get fives() {
+    return this.#countValue(5);
+  }
+
+  get sixes() {
+    return this.#countValue(6);
+  }
+
+  get full_house() {
+    const isFullHouse = [2, 3].every((number) =>
+      [...new Set(this.#dice)].some(
+        (uniqueVal) =>
+          this.#dice.filter((die) => die === uniqueVal).length === number
+      )
+    );
+
+    if (isFullHouse) {
+      return this.choice;
+    } else {
+      return 0;
+    }
+  }
+
+  get four_of_a_kind() {}
+
+  get little_straight() {
+    if (!this.#dice.includes(6) && new Set(this.#dice).size === 5) {
+      return 30;
+    } else {
+      return 0;
+    }
+  }
+
+  get big_straight() {
+    if (!this.#dice.includes(1) && new Set(this.#dice).size === 5) {
+      return 30;
+    } else {
+      return 0;
+    }
+  }
+
+  get choice() {
+    return this.#dice.reduce((a, b) => a + b, 0);
+  }
+
+  get yacht() {
+    if (new Set(this.#dice).size === 1) {
+      return 50;
+    } else {
+      return 0;
+    }
+  }
+}
