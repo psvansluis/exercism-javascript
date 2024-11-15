84326f12ccb676fa827dd7e2e700b88f4a795e37
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2024-11-15 15:18:36 +0100

    killer sudoku helper

--- killer-sudoku-helper/killer-sudoku-helper.js
+++ killer-sudoku-helper/killer-sudoku-helper.js
@@ -1,8 +1,35 @@
-//
-// This is only a SKELETON file for the 'Killer Sudoku Helper' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// @ts-check
 
-export const combinations = (cage) => {
-  throw new Error('Remove this statement and implement this function');
+const ONE_TO_NINE = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+/**
+ *
+ * @param {{sum: number, size: number, exclude: number[]}} cage
+ * @returns {number[][]}
+ */
+export const combinations = ({ sum, size, exclude }) =>
+  subsets(
+    ONE_TO_NINE.filter((num) => !exclude.includes(num)),
+    size
+  ).filter((set) => set.reduce((a, b) => a + b, 0) === sum);
+
+/**
+ * @template T
+ * @param {T[]} set
+ * @param {number} size
+ * @returns {T[][]}
+ */
+const subsets = (set, size) => {
+  let result = [];
+  const comb = (subsetAcc, rest, n) => {
+    if (n === 0) {
+      result.push(subsetAcc);
+    } else {
+      for (let index = 0; index < rest.length; index++) {
+        comb([...subsetAcc, rest[index]], rest.slice(index + 1), n - 1);
+      }
+    }
+  };
+  comb([], set, size);
+  return result;
 };
